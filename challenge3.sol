a599237b99106ce905ed28647b41846840312e69
Abdul Sami <isamjay007@gmail.com>
2020-11-14 02:55:13 +0500

    initial commit

---
+++ challenge3.sol
@@ -0,0 +1,143 @@
+pragma solidity ^0.6.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
+}
+
+contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+library SafeMath {
+/**
+    * @dev Multiplies two numbers, throws on overflow.
+    */
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        // Gas optimization: this is cheaper than asserting 'a' not being zero, but the
+        // benefit is lost if 'b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (a == 0) {
+        return 0;
+        }
+    c = a * b;
+        assert(c / a == b);
+        return c;
+    }
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        // uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return a / b;
+    }
+    /**
+    * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
+    */
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+    /**
+    * @dev Adds two numbers, throws on overflow.
+    */
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        assert(c >= a);
+        return c;
+    }
+}
+
+contract challenge3 is Ownable{
+    
+    using SafeMath for uint256;
+    
+    enum TaskStatus {
+        Incomplete,
+        Complete
+    }
+    
+    mapping(uint256 => Tasks) public todoList;
+    
+    struct Tasks {
+        string name;
+        TaskStatus status;
+    }
+    
+    uint256 counter;
+    
+    constructor() public {
+        counter = 0;
+    }
+    
+    function addTask(string calldata _name) onlyOwner external {
+        todoList[counter].name = _name;
+        todoList[counter].status = TaskStatus.Incomplete;
+        counter = counter.add(1);
+    }
+    
+    function toggleTask(uint256 _index) onlyOwner external {
+        if(todoList[_index].status == TaskStatus.Incomplete){
+            todoList[_index].status = TaskStatus.Complete;
+        } else{
+            todoList[_index].status = TaskStatus.Incomplete;
+        }
+    }
+    
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# BlockXScoiety challenge 3
+
+This repository contains a solidity file which is a solution to a challenge posted by blockXSociety. The contract allows an owner to create a todo-list and update their status.
